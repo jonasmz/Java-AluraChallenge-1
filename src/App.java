8f5ea71675d0846d507dc208c28ac3fb26b090a0
Jonás Muñiz <jonasmz@gmail.com>
2023-03-06 16:14:00 -0300

    Added example of using CurrencyConverter

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import Services.CurrencyConverter.CurrencyConverter;
 import Services.NumericalConverter.NumericalBaseEnum;
 import Services.NumericalConverter.NumericalConverter;
 
@@ -11,5 +12,21 @@ public class App {
 
        System.out.println("From Base \t value \t | \t result \t to base");
        System.out.println(fromBase + " \t " + value + " \t | \t " + result + " \t " + toBase);
+
+       CurrencyConverter currencyConverter = new CurrencyConverter();
+       currencyConverter.setServiceApikey("NCDLawh1haw66ao4PqvQxjWZ8xmo6gRr");
+       var fromCurrency = "USD";
+       var toCurrency = "Ars";
+       var amount = 1d;
+
+       var currencyResult = currencyConverter.Convert(fromCurrency, toCurrency, amount);
+       if(Boolean.valueOf(currencyResult.getSuccess())){
+           System.out.println("From Currency \t Amount \t | \t Result \t To Currency");
+           System.out.println(fromCurrency + " \t\t " + amount + " \t\t | \t " + currencyResult.getResult() + " \t " + toCurrency);
+       }else{
+            System.out.println("La peticion no se ha podido completar");
+            System.out.println("Error code: " + currencyResult.getError().getCode());
+            System.out.println("Error info: " + currencyResult.getError().getInfo());
+       }
     }
 }
